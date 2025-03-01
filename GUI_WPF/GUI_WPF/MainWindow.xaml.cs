22c9368194d63a1b5efa05ef168d9dbd2192e12f
praveentecsl <praveentecsl@gmail.com>
2025-03-01 12:28:56 +0530

    desktop application using .net

---
+++ GUI_WPF/GUI_WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new WelcomePage());
+
+
+        }
+
+        private void NavigateToHomePage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new HomePage());
+        }
+
+        private void NavigateToTouristsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new TouristsPage());
+        }
+
+        private void NavigateToBookingsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new BookingsPage());
+        }
+
+        private void NavigateToReportsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ReportsPage());
+        }
+        private void NavigateToWelcomePage(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new WelcomePage());
+        }
+    }
+
+}
---
+++ GUI_WPF/GUI_WPF/WelcomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_WPF
+{
+    /// <summary>
+    /// Interaction logic for WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignupButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignupWindow signupWindow = new SignupWindow();
+            signupWindow.ShowDialog();
+        }
+    }
+
+}
